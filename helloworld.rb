fb71201b3e9a2f7237c013d7ca25f0f03c8938b2
Nico Hartto <nico.hartto@gmail.com>
2011-03-28 04:06:06 +0000

    uusin versio

---
+++ helloworld.rb
@@ -0,0 +1,16 @@
+#
+# Example file for Cloud9 IDE presentation
+#
+
+class Helloworld
+    def greet
+        puts "Hello World!"
+    end
+end
+
+hello = Helloworld.new
+hello.greet * 2
+
+# More greets
+
+hello.greet * 2
